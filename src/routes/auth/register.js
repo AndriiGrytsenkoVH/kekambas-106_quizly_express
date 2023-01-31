812cdd9db2e00bd16184fd6609e21642c05126ae
AndriiGrytsenkoVH <andrew.grit.vh@gmail.com>
2023-01-31 13:56:38 -0600

    pasward hashing

--- src/routes/auth/register.js
+++ src/routes/auth/register.js
@@ -1,4 +1,16 @@
-module.exports = (req, res) => {
+const { User } = require('../../models');
+const bcrypt = require('bcrypt')
+
+
+module.exports = async (req, res) => {
     console.log(req.body)
-    res.end('Post Auth Router Register')
+    if (req.body.password !== req.body.confirmPass){
+        res.send({ error: 'Your Passwords do not match'})
+    } else {
+        const { username, email, password } = req.body
+        const passwordHash = await bcrypt.hash(password, 10)
+        const user = new User({ username, email, password: passwordHash})
+        user.save()
+        res.send(`New User Created - ${user.username}`)
+    }
 }
