ffdeee7d5ba1bc4fbd4366bc5509205437e578c8
AndriiGrytsenkoVH <andrew.grit.vh@gmail.com>
2023-01-31 11:53:36 -0600

    mongo db

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+const dotenv = require('dotenv')
+dotenv.config();
 const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const { connectDB } = require('./src/db');
+
+connectDB();
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'src/templates/views'))
+
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
     res.send('Hello World')
