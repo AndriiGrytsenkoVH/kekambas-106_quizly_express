3c75ead8a26092992a53043442c2d42106945812
AndriiGrytsenkoVH <andrew.grit.vh@gmail.com>
2023-02-01 12:20:14 -0600

    new register

--- src/routes/auth/register.js
+++ src/routes/auth/register.js
@@ -1,5 +1,6 @@
 const { User } = require('../../models');
 const bcrypt = require('bcrypt')
+const axios = require('axios')
 
 
 module.exports = async (req, res) => {
@@ -7,10 +8,34 @@ module.exports = async (req, res) => {
     if (req.body.password !== req.body.confirmPass){
         res.send({ error: 'Your Passwords do not match'})
     } else {
-        const { username, email, password } = req.body
-        const passwordHash = await bcrypt.hash(password, 10)
-        const user = new User({ username, email, password: passwordHash})
-        user.save()
-        res.send(`New User Created - ${user.username}`)
+        try{
+            const mutation = `
+            mutation ($email: String!, $username: String!, $password: String!){
+                register(email: $email, $username: $username, password: $password)
+            }`
+            const {data} = await axios.post(process.env.GRAPHQL_ENDPOINT,
+                {
+                    query: mutation,
+                    variables: {
+                        email: req.body.email,
+                        username: req.body.username,
+                        password: req.body.password
+                    }
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+            )
+
+            const jwtToken = data.data.register
+            console.log(jwtToken)
+            res.redirect('/')
+
+        } catch(err) {
+            console.log(err)
+            res.redirect('/auth/register')
+        }
     }
 }
